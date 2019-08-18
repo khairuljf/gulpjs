4e81a0ef0022603010093ecd7b9c0e32bebb7633
Khairul <khairuljf@gmail.com>
2019-08-18 12:27:15 +0600

    Update gulp boilerplace

--- README.md
+++
@@ -1,2 +0,0 @@
-# gulpjs practice files
-
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,46 @@
-const gulp = require( 'gulp' );
-const rename = require( 'gulp-rename' );
-const sass = require( 'gulp-sass' );
-const autoprefixer = require( 'gulp-autoprefixer' );
-const sourcemaps = require( 'gulp-sourcemaps' );
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    uglify = require('gulp-uglify-es').default,
+    cleancss = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel = require('gulp-babel')
 
 
-var styleSRC = 'src/scss/style.scss';
-var  targetStyle= 'assets/css';
+gulp.task('sass', function(){
+    return gulp.src( 'src/assets/scss/*.scss' )
+        .pipe(sourcemaps.init())
+        .pipe( sass() )
+        .pipe( cleancss() )
+        .pipe(sourcemaps.write(''))
+        .pipe( gulp.dest( 'assets/css'));
+});
 
-var jsSRC = 'src/js/custom.js';
-var  targetjs= 'assets/js';
+gulp.task('frontend-js', function() {
+    return gulp.src( 'src/assets/scripts/frontend/*.js' )
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe( concat( 'main.js'))
+        .pipe( uglify())
+        .pipe(sourcemaps.write(''))
+        .pipe( gulp.dest( 'assets/js'));
+} );
+gulp.task('admin-js', function() {
+    return gulp.src( 'src/assets/scripts/admin/*.js' )
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(concat( 'combined-admin.js' ))
+        .pipe( uglify())
+        .pipe(sourcemaps.write(''))
+        .pipe( gulp.dest( 'assets/js' ) );
+} );
 
-gulp.task('style', sytleCB);
-
-function sytleCB(){
-    gulp.src( styleSRC )
-        .pipe( sourcemaps.init() )
-        .pipe( sass({
-            errLogToConsole: true,
-            outputStyle: 'compressed',
-        }) )
-        .on( 'error', console.error.bind( console ) )
-        .pipe( autoprefixer( {
-            browsers: ['last 2 versions'],
-            cascade: false
-        } ) )
-        .pipe( rename( { suffix: '.min' } ) )
-        .pipe( sourcemaps.write( './' ) )
-        .pipe( gulp.dest( targetStyle ) );
-}
-
-gulp.task('js', js_cb);
-
-
-function js_cb(){
-    gulp.src( jsSRC )
-        .pipe( gulp.dest( targetjs ) );
-}
-
-
-gulp.task('default', function(){
-    js_cb();
-    sytleCB();
+gulp.task('watch', function() {
+    gulp.watch( 'src/assets/scss/**/*.scss',  gulp.series('sass'));
+    gulp.watch( 'src/assets/scripts/frontend/*.js', gulp.series('frontend-js'));
+    gulp.watch( 'src/assets/scripts/admin/*.js', gulp.series('admin-js'));
 });
