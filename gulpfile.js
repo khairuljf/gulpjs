dc9019dacec7014a5423d099744b946e2778a8d0
khairul Islam <khairul@orangetoolz.com>
2020-02-25 13:01:51 +0600

    Updated files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass'),
     uglify = require('gulp-uglify-es').default,
     cleancss = require('gulp-clean-css'),
     concat = require('gulp-concat'),
@@ -7,17 +8,20 @@ var gulp = require('gulp'),
     babel = require('gulp-babel')
 
 
-gulp.task('sass', function(){
-    return gulp.src( 'src/assets/scss/*.scss' )
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(['src/assets/scss/*.scss'])
         .pipe(sourcemaps.init())
         .pipe( sass() )
         .pipe( cleancss() )
         .pipe(sourcemaps.write(''))
-        .pipe( gulp.dest( 'assets/css'));
+        .pipe( gulp.dest( 'assets/css'))
+        .pipe(browserSync.stream());
 });
 
+// Move the javascript files into our /src/js folder
 gulp.task('frontend-js', function() {
-    return gulp.src( 'src/assets/scripts/frontend/*.js' )
+    return gulp.src([ 'src/assets/scripts/*.js'])
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['@babel/env']
@@ -25,22 +29,22 @@ gulp.task('frontend-js', function() {
         .pipe( concat( 'main.js'))
         .pipe( uglify())
         .pipe(sourcemaps.write(''))
-        .pipe( gulp.dest( 'assets/js'));
-} );
-gulp.task('admin-js', function() {
-    return gulp.src( 'src/assets/scripts/admin/*.js' )
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat( 'combined-admin.js' ))
-        .pipe( uglify())
-        .pipe(sourcemaps.write(''))
-        .pipe( gulp.dest( 'assets/js' ) );
-} );
-
-gulp.task('watch', function() {
-    gulp.watch( 'src/assets/scss/**/*.scss',  gulp.series('sass'));
-    gulp.watch( 'src/assets/scripts/frontend/*.js', gulp.series('frontend-js'));
-    gulp.watch( 'src/assets/scripts/admin/*.js', gulp.series('admin-js'));
+        .pipe( gulp.dest( 'assets/js'))
+       .pipe(browserSync.stream());
 });
+
+// Static Server + watching scss/js/html files
+gulp.task('serve', gulp.series('sass','frontend-js', function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch(['src/assets/scss/*.scss'], gulp.series('sass'));
+    gulp.watch(['src/assets/scripts/*.js'], gulp.series('frontend-js'));
+    gulp.watch("*.html").on('change', browserSync.reload);
+}));
+
+gulp.task('default', gulp.parallel('frontend-js','serve'));
+
+
